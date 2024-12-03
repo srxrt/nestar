3ad096df8358af507ed6178b3c9e1e37a55ab6c6
srxrt <str9703@gmail.com>
2024-12-04 05:07:21 +0900

    fix: modify member.resolver

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -1,6 +1,5 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { MemberService } from './member.service';
-import { InternalServerErrorException } from '@nestjs/common';
 import { LoginInput, MemberInput } from '../../libs/dto/member/member.input';
 import { Member } from '../../libs/dto/member/member';
 
@@ -10,24 +9,14 @@ export class MemberResolver {
 
 	@Mutation(() => Member)
 	public async signup(@Args('input') input: MemberInput): Promise<Member> {
-		try {
-			console.log('Mutation: signup');
-			return await this.memberService.signup(input);
-		} catch (err) {
-			console.log('Error: signup:', err);
-			throw new InternalServerErrorException(err);
-		}
+		console.log('Mutation: signup');
+		return await this.memberService.signup(input);
 	}
 
 	@Mutation(() => Member)
 	public async login(@Args('input') input: LoginInput): Promise<Member> {
-		try {
-			console.log('Mutation: login');
-			return await this.memberService.login(input);
-		} catch (err) {
-			console.log('Error: login:', err);
-			throw new InternalServerErrorException(err);
-		}
+		console.log('Mutation: login');
+		return await this.memberService.login(input);
 	}
 
 	@Mutation(() => String)
