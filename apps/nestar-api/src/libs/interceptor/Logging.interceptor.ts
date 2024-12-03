0635cb397330effaff6fe8ad6f0747b16452eb8b
srxrt <str9703@gmail.com>
2024-12-03 13:27:13 +0900

    feat: develop logging via global interceptors

---
+++ apps/nestar-api/src/libs/interceptor/Logging.interceptor.ts
@@ -0,0 +1,31 @@
+import { CallHandler, ExecutionContext, Injectable, Logger, NestInterceptor } from '@nestjs/common';
+import { GqlContextType, GqlExecutionContext } from '@nestjs/graphql';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+@Injectable()
+export class LoggingInterceptor implements NestInterceptor {
+	private readonly logger: Logger = new Logger();
+	public intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+		const recordTime = Date.now();
+		const requestType = context.getType<GqlContextType>();
+		if (requestType === 'http') {
+			// Develop if needed
+		} else if (requestType === 'graphql') {
+			const gqlContext = GqlExecutionContext.create(context);
+
+			this.logger.log(`${this.stringify(gqlContext.getContext().req.body)}`, 'REQUEST');
+
+			return next.handle().pipe(
+				tap((context) => {
+					const responseTime = Date.now() - recordTime;
+					this.logger.log(`${this.stringify(context)} - ${responseTime}ms\n\n`, 'RESPONSE');
+				}),
+			);
+		}
+	}
+	private stringify(context: ExecutionContext): string {
+		console.log(typeof context);
+		return JSON.stringify(context).slice(0, 75);
+	}
+}
--- apps/nestar-api/src/main.ts
+++ apps/nestar-api/src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { LoggingInterceptor } from './libs/interceptor/Loggin.interceptor';
+import { LoggingInterceptor } from './libs/interceptor/Logging.interceptor';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
