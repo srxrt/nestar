8b7d2511240c3d6f2f13c8fdd6d47f78ca676db2
srxrt <str9703@gmail.com>
2024-12-17 10:40:19 +0900

    feat: develop createBoardArticle graphql api

--- apps/nestar-api/src/components/board-article/board-article.resolver.ts
+++ apps/nestar-api/src/components/board-article/board-article.resolver.ts
@@ -1,7 +1,36 @@
-import { Resolver } from '@nestjs/graphql';
+import { Query, Args, Mutation, Resolver } from '@nestjs/graphql';
 import { BoardArticleService } from './board-article.service';
+import { BoardArticle } from '../../libs/dto/board-article/board-article';
+import { BoardArticleInput } from '../../libs/dto/board-article/board-article.input';
+import { AuthMember } from '../auth/decorators/authMember.decorator';
+import { ObjectId } from 'mongoose';
+import { UseGuards } from '@nestjs/common';
+import { AuthGuard } from '../auth/guards/auth.guard';
+import { WithoutGuard } from '../auth/guards/without.guard';
+import { shapeIntoMongoObjectId } from '../../libs/config';
 
 @Resolver()
 export class BoardArticleResolver {
 	constructor(private readonly boardArticleSerivce: BoardArticleService) {}
+
+	@UseGuards(AuthGuard)
+	@Mutation(() => BoardArticle)
+	public async createBoardArticle(
+		@Args('input') input: BoardArticleInput,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<BoardArticle> {
+		console.log('Mutation: createBoardArticle');
+		return await this.boardArticleSerivce.createBoardArticle(memberId, input);
+	}
+
+	@UseGuards(WithoutGuard)
+	@Query(() => BoardArticle)
+	public async getBoardArticle(
+		@Args('aricleId') input: string,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<BoardArticle> {
+		console.log('Query: getBoardArticle');
+		const articleId = shapeIntoMongoObjectId(input);
+		return await this.boardArticleSerivce(memberId, articleId);
+	}
 }
--- apps/nestar-api/src/components/board-article/board-article.service.ts
+++ apps/nestar-api/src/components/board-article/board-article.service.ts
@@ -1,9 +1,30 @@
-import { Injectable } from '@nestjs/common';
-import { Model } from 'mongoose';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { Model, ObjectId } from 'mongoose';
 import { BoardArticle } from '../../libs/dto/board-article/board-article';
 import { InjectModel } from '@nestjs/mongoose';
+import { BoardArticleInput } from '../../libs/dto/board-article/board-article.input';
+import { Message } from '../../libs/enums/common.enum';
+import { MemberService } from '../member/member.service';
+import { ViewService } from '../view/view.service';
 
 @Injectable()
 export class BoardArticleService {
-	constructor(@InjectModel('BoardArticle') private readonly boardArticleModel: Model<BoardArticle>) {}
+	constructor(
+		@InjectModel('BoardArticle') private readonly boardArticleModel: Model<BoardArticle>,
+		private memberService: MemberService,
+		private viewService: ViewService,
+	) {}
+
+	public async createBoardArticle(memberId: ObjectId, input: BoardArticleInput): Promise<BoardArticle> {
+		input.memberId = memberId;
+		try {
+			const result = await this.boardArticleModel.create(input);
+			await this.memberService.memberStatsModifier({ _id: memberId, targetKey: 'memberArticles', modifier: 1 });
+			return result;
+		} catch (err) {
+			console.log('Board Article Service: createBoardArticle', err.message);
+			throw new InternalServerErrorException(Message.CREATE_FAILED);
+		}
+	}
+	public async getBoardArticle(memberId: ObjectId, articleId: ObjectId): Promise<BoardArticle> {}
 }
