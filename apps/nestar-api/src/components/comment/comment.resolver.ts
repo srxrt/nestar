3c5a3f4a4b3cfeea90401572304434e460abe81e
srxrt <str9703@gmail.com>
2024-12-18 12:29:09 +0900

    feat: develop getComments graphql api

--- apps/nestar-api/src/components/comment/comment.resolver.ts
+++ apps/nestar-api/src/components/comment/comment.resolver.ts
@@ -1,13 +1,14 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { CommentService } from './comment.service';
-import { Comment } from '../../libs/dto/comment/comment';
+import { Comment, Comments } from '../../libs/dto/comment/comment';
 import { UseGuards } from '@nestjs/common';
 import { AuthGuard } from '../auth/guards/auth.guard';
-import { CommentInput } from '../../libs/dto/comment/comment.input';
+import { CommentInput, CommentsInquiry } from '../../libs/dto/comment/comment.input';
 import { AuthMember } from '../auth/decorators/authMember.decorator';
 import { ObjectId } from 'mongoose';
 import { shapeIntoMongoObjectId } from '../../libs/config';
 import { CommentUpdate } from '../../libs/dto/comment/comment.update';
+import { WithoutGuard } from '../auth/guards/without.guard';
 
 @Resolver()
 export class CommentResolver {
@@ -32,5 +33,16 @@ export class CommentResolver {
 		console.log('Mutation: updateComment');
 		input._id = shapeIntoMongoObjectId(input._id);
 		return await this.commentService.updateComment(memberId, input);
+	}
+
+	@UseGuards(WithoutGuard)
+	@Query(() => Comments)
+	public async getComments(
+		@Args('input') input: CommentsInquiry,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Comments> {
+		console.log('Query: getComments');
+		input.search.commentRefId = shapeIntoMongoObjectId(input.search.commentRefId);
+		return await this.commentService.getComments(memberId, input);
 	}
 }
--- apps/nestar-api/src/components/comment/comment.service.ts
+++ apps/nestar-api/src/components/comment/comment.service.ts
@@ -1,14 +1,14 @@
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model, ObjectId } from 'mongoose';
-import { Comment } from '../../libs/dto/comment/comment';
+import { Comment, Comments } from '../../libs/dto/comment/comment';
 import { MemberService } from '../member/member.service';
 import { PropertyService } from '../property/property.service';
-import { CommentInput } from '../../libs/dto/comment/comment.input';
+import { CommentInput, CommentsInquiry } from '../../libs/dto/comment/comment.input';
 import { BoardArticleService } from '../board-article/board-article.service';
 import { CommentGroup, CommentStatus } from '../../libs/enums/comment.enum';
-import { Message } from '../../libs/enums/common.enum';
-import { shapeIntoMongoObjectId } from '../../libs/config';
+import { Direction, Message } from '../../libs/enums/common.enum';
+import { lookupMember, shapeIntoMongoObjectId } from '../../libs/config';
 import { CommentUpdate } from '../../libs/dto/comment/comment.update';
 import { T } from '../../libs/types/common';
 
@@ -58,7 +58,7 @@ export class CommentService {
 	public async updateComment(memberId: ObjectId, input: CommentUpdate): Promise<Comment> {
 		const search: T = { _id: input._id, memberId: memberId, commentStatus: CommentStatus.ACTIVE };
 		const { commentStatus } = input;
-		const result = await this.commentModel.findOneAndUpdate(search, input, { new: true });
+		const result = await this.commentModel.findOneAndUpdate(search, input, { new: true }).exec();
 		if (!result) throw new InternalServerErrorException(Message.UPDATE_FAILED);
 		if (commentStatus === CommentStatus.DELETE) {
 			switch (result.commentGroup) {
@@ -83,5 +83,29 @@ export class CommentService {
 			}
 		}
 		return result;
+	}
+
+	public async getComments(memberId: ObjectId, input: CommentsInquiry): Promise<Comments> {
+		const { commentRefId } = input.search;
+		console.log('COmment ref id', commentRefId);
+		const match: T = { commentRefId: commentRefId, commentStatus: CommentStatus.ACTIVE };
+		const sort: T = { [input?.sort ?? 'createdAt']: input?.direction ?? Direction.DESC };
+		const result: Comments[] = await this.commentModel.aggregate([
+			{ $match: match },
+			{ $sort: sort },
+			{
+				$facet: {
+					list: [
+						{ $skip: (input.page - 1) * input.limit },
+						{ $limit: input.limit },
+						lookupMember,
+						{ $unwind: '$memberData' },
+					],
+					metaCounter: [{ $count: 'total' }],
+				},
+			},
+		]);
+		if (!result.length) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+		return result[0];
 	}
 }
