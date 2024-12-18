48ffffb2ed6c71f862f3933ec455624853992490
srxrt <str9703@gmail.com>
2024-12-18 10:46:22 +0900

    feat: configure comment module, create comment resolver and service

--- apps/nestar-api/src/components/comment/comment.module.ts
+++ apps/nestar-api/src/components/comment/comment.module.ts
@@ -1,4 +1,20 @@
 import { Module } from '@nestjs/common';
+import { CommentResolver } from './comment.resolver';
+import { CommentService } from './comment.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from '../auth/auth.module';
+import { MemberModule } from '../member/member.module';
+import { ViewModule } from '../view/view.module';
+import CommentSchema from '../../schemas/Comment.model';
+import { PropertyModule } from '../property/property.module';
 
-@Module({})
+@Module({
+	imports: [
+		MongooseModule.forFeature([{ name: 'Comment', schema: CommentSchema }]),
+		AuthModule,
+		PropertyModule,
+		MemberModule,
+	],
+	providers: [CommentResolver, CommentService],
+})
 export class CommentModule {}
