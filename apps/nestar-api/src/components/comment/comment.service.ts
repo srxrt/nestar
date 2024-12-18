7780672152ae80e272a6e3631ec134886b84accd
srxrt <str9703@gmail.com>
2024-12-18 12:40:20 +0900

    feat: develop removeCommentByAdmin

--- apps/nestar-api/src/components/comment/comment.service.ts
+++ apps/nestar-api/src/components/comment/comment.service.ts
@@ -108,4 +108,10 @@ export class CommentService {
 		if (!result.length) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
 		return result[0];
 	}
+
+	public async removeCommentByAdmin(commentId: ObjectId): Promise<Comment> {
+		const result = await this.commentModel.findByIdAndDelete(commentId).exec();
+		if (!result) throw new InternalServerErrorException(Message.REMOVE_FAILED);
+		return result;
+	}
 }
